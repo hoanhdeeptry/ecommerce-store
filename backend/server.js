4d6b23cd2250af89ab135ab3520fcbeb818f6dd4
Hoang Anh <adiep05@gmail.com>
2024-12-12 01:01:32 +0700

    update code

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 //auth
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
 
