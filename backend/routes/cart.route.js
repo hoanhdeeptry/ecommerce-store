3ae3f8108bc38fb89ad56ae8088feed34691d516
toadvine6644 <tikkboi@gmail.com>
2024-12-19 21:11:08 +0700

    fix

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,6 +1,11 @@
 import express from "express";
-import { addToCart, getCartProducts, removeAllFromCart, updateQuantity } from "../controllers/cart.controller.js";
-import { protectRoute } from "../../middleware/auth.middleware.js";
+import {
+  addToCart,
+  getCartProducts,
+  removeAllFromCart,
+  updateQuantity,
+} from "../controllers/cart.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
