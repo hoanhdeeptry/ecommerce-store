efb3e9e0c287b13510b713dbd7da34d279032bdb
Hoang Anh <adiep05@gmail.com>
2024-12-29 15:54:03 +0700

    update code

--- backend/controllers/analytics.controller.js
+++ backend/controllers/analytics.controller.js
@@ -1,6 +1,6 @@
-import Order from "../models/order.model.js";
-import Product from "../models/product.model.js";
-import User from "../models/user.model.js";
+import Order from "../model/order.model.js";
+import Product from "../model/product.model.js";
+import User from "../model/user.model.js";
 
 export const getAnalyticsData = async () => {
 	const totalUsers = await User.countDocuments();
